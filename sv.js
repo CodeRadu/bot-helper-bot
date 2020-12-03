cd2c95cf00d7bd083be2ad74f3f8380bdb9810f7
papucicumuci <47945947+papucicumuci@users.noreply.github.com>
2020-12-03 18:23:55 +0200

    Updated

--- sv.js
+++ sv.js
@@ -2,6 +2,7 @@ const express=require('express')
 const app=express()
 const server=require('http').Server(app)
 const exec=require('child_process').exec
+const io=require('socket.io')(server)
 
 app.use(express.static('public'))
 
