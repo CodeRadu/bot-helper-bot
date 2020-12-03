afa679ada98604b7d8cb693372544cf454e36c7f
papucicumuci <47945947+papucicumuci@users.noreply.github.com>
2020-12-03 19:25:48 +0200

    Updated

--- bot.js
+++ bot.js
@@ -180,7 +180,7 @@ bot.on('message', message=>{
     }
     if(prefix=='.'&&!message.author.bot){
         if(args[0]=="ping"){
-            message.reply(`🏓Latency is ${Date.now() - message.createdTimestamp}ms. API Latency is ${Math.round(bot.ws.ping)}ms`)
+            message.reply(`🏓Pong! ${Date.now() - message.createdTimestamp}ms`)
         }
     }
 })
